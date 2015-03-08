b587c24685c86840080fde97baab644e52b59427
Alex Bunardzic <macich@Alexs-Mac-mini.local>
2015-03-08 13:13:45 -0700

    layout improvements and integration tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,4 +16,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_response :success
   end
 
+  test "should get contact" do
+  	get :contact
+  	assert_response :success
+  	#assert_select "title", "Contact | Estuary"
+  end
+
 end
