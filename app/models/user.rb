b6c75f9b304f37c6be23e62001cd976d785f4f1a
Alex Bunardzic <macich@Alexs-Mac-mini.local>
2015-03-09 09:24:06 -0700

    Basic User model with secure password

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }
+end
