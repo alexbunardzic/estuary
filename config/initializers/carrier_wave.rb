1d35103c052698b38b6f5e75d15d51201630ab62
Alex Bunardzic <macich@Alexs-Mac-mini.local>
2015-03-12 13:22:35 -0700

    configured carrier wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
 
-#if Rails.env.production?
- # CarrierWave.configure do |config|
-  #  config.fog_credentials = {
-   #   # Configuration for Amazon S3
-    #  :provider              => 'AWS',
-     # :aws_access_key_id     => ENV['AKIAJB4VUHPWAVDPNPAQ'],
-      #:aws_secret_access_key => ENV['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']
-    #}
-    #config.fog_directory     =  ENV['estuary']
-  #end
-#end
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAIHFLED6BXN5O3DJA'],
+      :aws_secret_access_key => ENV['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']
+    }
+    config.fog_directory     =  ENV['estuary']
+  end
+end
