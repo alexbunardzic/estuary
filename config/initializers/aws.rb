7127fa0293b3796aa1c2407606a2c1ab3e46b5de
Alex Bunardzic <macich@Alexs-Mac-mini.local>
2015-03-12 18:53:47 -0700

    commented out carrier wave

---
+++ config/initializers/aws.rb
@@ -0,0 +1,4 @@
+#AWS.config(access_key_id: ENV['AKIAIFBYQOK2ZGZ2XM3Q'],
+  #         secret_access_key: ENV['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'] )
+
+#S3_BUCKET = AWS::S3.new.buckets[ENV['estuarybucket']]
